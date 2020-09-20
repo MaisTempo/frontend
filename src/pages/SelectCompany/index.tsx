6b0e8eff90b611944076c3148bb878aa1990d78a
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-19 23:33:13 -0300

    feat: adiciona pagina seleção empresa

---
+++ src/pages/SelectCompany/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+const SelectCompany: React.FC = () => {
+  return (
+    <Header />
+  );
+}
+
+export default SelectCompany;
