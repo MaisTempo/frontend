ed04c9856cdc1476f3b9dfcd8bacd78c33f82152
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-20 02:11:41 -0300

    fix: arruma tamanho svg

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+import { FiSearch } from 'react-icons/fi';
+import { Container } from './styles';
+
+const Input: React.FC = () => {
+  return (
+    <Container>
+      <FiSearch size={25} color="FB7E21"/>
+      <input/>
+    </Container>
+  );
+}
+
+export default Input;
