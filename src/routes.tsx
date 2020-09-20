32d4d62963f6a01687490adfeba10e45bc24311c
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-20 19:03:32 -0300

    feat: adiciona página home

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,13 +5,15 @@ import SelectCompany from './pages/SelectCompany';
 import SelectHelp from './pages/SelectHelp';
 import FinalityPage from './pages/FinalityPage';
 import StepByStep from './pages/StepByStep';
+import Home from './pages/Home';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
-    <Route path="/" exact component={SelectCompany}/>
+    <Route path="/" exact component={Home}/>
+    <Route path="/start" exact component={SelectCompany}/>
     <Route path="/finalidades" exact component={SelectHelp}/>
     <Route path="/finalidades/escolha" exact component={FinalityPage}/>
-    <Route path="/finalidades/escolha/passoapasso" component={StepByStep}/>
+    <Route path="/finalidades/escolha/passoapasso" exact component={StepByStep}/>
   </BrowserRouter>
 );
 
