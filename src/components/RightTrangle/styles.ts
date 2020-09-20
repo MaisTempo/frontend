d0e0859494a0e41473fe028f32ac66e086776c62
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-20 16:18:04 -0300

    feat: adiciona triangulo direito

--- src/components/RightTrangle/styles.ts
+++ src/components/RightTrangle/styles.ts
@@ -9,14 +9,14 @@ export const Container = styled.div<ContainerProps>`
   height: 0; 
 
   ${props => props.color === 'fff' && css `
-    border-left: 30px solid transparent;
-    border-right: 30px solid transparent;
-    border-top: 30px solid #fff
+  border-top: 15px solid transparent;
+  border-bottom: 15px solid transparent;
+  border-left: 15px solid #fff;
   `}
 
   ${props => props.color === 'FB6C02' && css `
-    border-left: 15px solid transparent;
-    border-right: 15px solid transparent;
-    border-top: 15px solid #FB6C02
+  border-top: 15px solid transparent;
+  border-bottom: 15px solid transparent;
+  border-left: 15px solid #FB6C02;
   `}  
 `;
