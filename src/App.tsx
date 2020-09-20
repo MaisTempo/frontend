322bf212799ab4f0457285475bd1e738e648af24
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-20 02:14:15 -0300

    fix: arrumado roteamento de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <GlobalStyle>
+  <Router>
     <Routes />
-  </GlobalStyle>
+    <GlobalStyle />
+  </Router>
 );
 
 export default App;
