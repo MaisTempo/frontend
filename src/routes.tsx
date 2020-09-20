5570fdf3dd339345024939f337fbf6d0d3f80774
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-19 23:46:22 -0300

    fix: controlador de rotas arrumado

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-function Routes() {
-  return (
-    <BrowserRouter>
-      <Route path="/" exact component={"Home"}/>
-    </BrowserRouter>
-  );
-}
+import SelectCompany from './pages/SelectCompany';
+
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Route path="/" exact component={SelectCompany}/>
+  </BrowserRouter>
+);
 
 export default Routes;
