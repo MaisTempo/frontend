605efe21b11c064a69d893ae312822597f38ed6a
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-20 12:38:59 -0300

    feat: adicionada nova rota

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import SelectCompany from './pages/SelectCompany';
+import SelectHelp from './pages/SelectHelp';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Route path="/" exact component={SelectCompany}/>
+    <Route path="/finalidades/:id" component={SelectHelp}/>
   </BrowserRouter>
 );
 
