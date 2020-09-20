1f2c8366a9835cf9a177a5ffe6525cbc667fa932
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-20 02:11:01 -0300

    feat: adiciona componente triânngulo

---
+++ src/components/Triangle/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Container } from './styles';
+
+const Triangle: React.FC = () => (
+  <Container /> 
+);
+
+export default Triangle;
---
+++ src/components/Triangle/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 0; 
+  height: 0; 
+  border-left: 30px solid transparent;
+  border-right: 30px solid transparent;
+  border-top: 30px solid #fff;
+`;
