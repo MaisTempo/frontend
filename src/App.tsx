665c89616fd98d2cf702442c1bb35c6486be4ffb
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-19 23:05:48 -0300

    feat: adiciona rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 const App: React.FC = () => (
   <GlobalStyle>
-    
+    <Routes />
   </GlobalStyle>
 );
 
