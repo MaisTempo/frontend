f0e3ed59047b49b220cb7d5515882403a7bd6c36
Gabriel Silva <gabrielsos99@gmail.com>
2020-09-20 21:08:55 -0300

    fix: mostra nome da finalidade

--- src/pages/SelectHelp/index.tsx
+++ src/pages/SelectHelp/index.tsx
@@ -50,12 +50,12 @@ const SelectHelp: React.FC = () => {
             <ul className="items-grid">
               {options.map(option => (
                 <li key={option._id}>
-                    <Link to="/finalidades/escolha"
-                            onClick={() => handleOptionClick(option._id)}>
-                      <LiContent>
+                    <Link to="/finalidades/escolha">     
+                      <LiContent onClick={() => handleOptionClick(option._id)}>
                         <span>{option.name}</span>
                           <RightTriangle
                             color={"FB6C02"}
+                            onClick={() => handleOptionClick(option._id)}
                           />
                       </LiContent>
                     </Link>
